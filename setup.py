3f4dd21232065b1f2ea4c44075f8dd050cb16d7b
SzateX <mistrzswiata.salata@gmail.com>
2016-05-23 20:20:10 +0200

    moved repository

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name = "virtual_score_board",
-    version = "0.0.1",
-    author = "Jakub Szatkowski",
-    author_email = "jaksza18@gmail.com",
-    description = ("A virtual scoreboard for presenting match details."),
-    install_requires = ['autobahn>=0.12.1', 'Twisted>=15.5.0', 'pytest>=2.8.7', 'click>=6.2', 'passlib>=1.6.5'],
-    license = "MiT",
-    keywords = "virtual scoreboard score sport",
-    packages = find_packages(),
+    name="virtual_score_board",
+    version="0.0.1",
+    author="Jakub Szatkowski",
+    author_email="jaksza18@gmail.com",
+    description=("A virtual scoreboard for presenting match details."),
+    install_requires=['autobahn>=0.12.1', 'Twisted>=15.5.0', 'pytest>=2.8.7', 'click>=6.2', 'passlib>=1.6.5'],
+    license="MiT",
+    keywords="virtual scoreboard score sport",
+    packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",

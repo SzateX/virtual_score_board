6769852c8309cb1c3625b27a0d88a267d8152d93
SzateX <mistrzswiata.salata@gmail.com>
2016-05-26 19:01:14 +0200

    Added responses from parser.

--- virtual_score_board/parser_responses.py
+++ virtual_score_board/parser_responses.py
@@ -1,38 +1,44 @@
-"""class InfoResponse(object):
+class Response(object):
+    def __init__(self):
+        self.type_of_response = None
+        self.code_of_response = None
+        self.status = None
+
+    def get_response(self):
+        dictionary = {"type": self.type_of_response,
+                      "code": self.code_of_response,
+                      "status": self.status
+                      }
+        return dictionary
+
+
+class Status(object):
     pass
 
 
-class WarningResponse(object):
+class EverythingGood(Status):
     pass
 
 
-class ErrorResponse(object):
-    pass"""
-
-
-class EverythingGood(object):
+class CorrectCredentials(Status):
     pass
 
 
-class CorrectCredentials(object):
+class SignMeOut(Status):
     pass
 
 
-class SignMeOut(object):
+class Pong(Status):
     pass
 
 
-class Pong(object):
+class CurrentlyLogged(Status):
     pass
 
 
-class CurrentlyLogged(object):
+class NotLogged(Status):
     pass
 
 
-class NotLogged(object):
-    pass
-
-
-class WrongCredentials(object):
+class WrongCredentials(Status):
     pass

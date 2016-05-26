21bc7c8bb391d6670a899b20cde1e10ee5873da0
SzateX <mistrzswiata.salata@gmail.com>
2016-05-26 21:33:34 +0200

    Added responses from parser.

--- virtual_score_board/parser_responses.py
+++ virtual_score_board/parser_responses.py
@@ -12,3 +12,38 @@ class Response(object):
                       "data": self.data
                       }
         return dictionary
+
+
+class EverythingGood(Response):
+    def __init__(self, data=None):
+        super(EverythingGood, self).__init__("Information", 1000, "EverythingGood", data)
+
+
+class CorrectCredentials(Response):
+    def __init__(self, data=None):
+        super(CorrectCredentials, self).__init__("Information", 1001, "CorrectCredentials", data)
+
+
+class SignedOut(Response):
+    def __init__(self, data=None):
+        super(SignedOut, self).__init__("Information", 1002, "SignedOut", data)
+
+
+class Pong(Response):
+    def __init__(self, data=None):
+        super(Pong, self).__init__("Information", 1999, "Pong", data)
+
+
+class NotLogged(Response):
+    def __init__(self, data=None):
+        super(NotLogged, self).__init__("Error", 3001, "NotLogged", data)
+
+
+class WrongCredentials(Response):
+    def __init__(self, data=None):
+        super(WrongCredentials, self).__init__("Error", 3002, "WrongCredentials", data)
+
+
+class CurrentlyLogged(Response):
+    def __init__(self, data=None):
+        super(CurrentlyLogged, self).__init__("Error", 3003, "CurrentlyLogged", data)

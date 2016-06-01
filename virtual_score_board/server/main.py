89d223db3cb6cb384f9fd9760bbe93b64dc4a646
SzateX <mistrzswiata.salata@gmail.com>
2016-06-01 22:20:04 +0200

    Corrected things in manager

--- virtual_score_board/server/main.py
+++ virtual_score_board/server/main.py
@@ -3,17 +3,22 @@ from virtual_score_board.server.handler import ServerHandler
 import sys
 from twisted.python import log
 from twisted.internet import reactor
-import click
+from virtual_score_board.config_manager import ConfigManager
+# import click
 
 
-@click.command()
-@click.option('--port', default=5000, help='Number of port (default=5000).')
-@click.option('--interface', default="0.0.0.0", prompt='Please enter interface ip (default=0.0.0.0)',
-              help='Ip of interface. (default=0.0.0.0)')
-def main(interface, port):
-    log.startLogging(sys.stdout)
+# "@click.command()
+# @click.option('--port', default=5000, help='Number of port (default=5000).')
+# @click.option('--interface', default="0.0.0.0", prompt='Please enter interface ip (default=0.0.0.0)',
+#             help='Ip of interface. (default=0.0.0.0)')
+def main():
+    config = ConfigManager()
+    config.read_config()
 
-    factory = WebSocketServerFactory(u"ws://%s:%s" % (interface, port))
+    # log.startLogging(sys.stdout)
+    log.startLogging(config.log_file_path)
+
+    factory = WebSocketServerFactory(u"%s://%s:%s" % ("wss" if config.use_ssl else "ws", config.host, config.port))
     factory.protocol = ServerHandler
     # factory.setProtocolOptions(maxConnections=2)
 
